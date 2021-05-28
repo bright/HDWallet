763a1d16c5dcf0bff53470934ac1f3e97508d18b
Bartek <bartek.rozwarski@brightinventions.com>
2021-05-28 09:39:06 +0200

    savepoint cosmos

--- Mobix/AppDelegate.swift
+++ Mobix/AppDelegate.swift
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        Cosmos.shared.fetchCosmosCoin()
+        Cosmos.shared.refreshBalance()
         appFlowController = AppFlowController(window!)
         appFlowController?.runFlow()
         appFlowController = AppFlowController(window!)
--- Mobix/Cosmos.swift
+++ Mobix/Cosmos.swift
@@ -10,79 +10,51 @@ enum CosmosError: Error {
     case couldNotFetchBalance
 }
 
+struct Wallet {
+    let address: String
+}
+//
+//class CosmosClient {
+//    static var shared = CosmosClient()
+//    let cosmos: Cosmos
+//    private init() {
+//        self.cosmos = Cosmos(provider: FetchAIMainnetProvider())
+//    }
+//
+//}
+
+protocol Provider {
+    var host: String {get}
+    var port: Int {get}
+}
+
+struct FetchAIMainnetProvider: Provider {
+    var host = "lcd-iris-app.cosmostation.io"
+    var port = 9090
+}
+
 class Cosmos {
-    
-    static var shared = Cosmos()
-    
     var publisher: AnyPublisher<Cosmos_Base_V1beta1_Coin?, Never> {
         // Here we're "erasing" the information of which type
         // that our subject actually is, only letting our outside
         // code know that it's a read-only publisher:
-        subject.eraseToAnyPublisher()
+        balanceSubject.eraseToAnyPublisher()
     }
-
+    let provider: Provider
     // By storing our subject in a private property, we'll only
     // be able to send new values to it from within this class:
-    private let subject = CurrentValueSubject<Cosmos_Base_V1beta1_Coin?, Never>(nil)
+    private let balanceSubject = CurrentValueSubject<Cosmos_Base_V1beta1_Coin?, Never>(nil)
     var address: String = "iaa1cv7mhcylar04wpxrtz6n6chtmh0lu6dl5d5dsq"
 
-    
-    private init() {}
-    
-    
-    func onFetchgRPCNodeInfo() {
-        DispatchQueue.global().async {
-            let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-            defer { try! group.syncShutdownGracefully() }
-            
-            let channel = self.getConnection(group)
-            defer { try! channel.close().wait() }
-            
-            let req = Cosmos_Base_Tendermint_V1beta1_GetNodeInfoRequest()
-            
-            do {
-                let response = try Cosmos_Base_Tendermint_V1beta1_ServiceClient(channel: channel).getNodeInfo(req).response.wait()
-                print(response)
-            } catch {
-                print("onFetchgRPCNodeInfo failed: \(error)")
-            }
-            
-        }
+    init(provider: Provider) {
+        self.provider = provider
     }
     
-    func getConnection(_ group: MultiThreadedEventLoopGroup) -> ClientConnection {
-        return ClientConnection.insecure(group: group).connect(host: "lcd-iris-app.cosmostation.io", port: 9090)
+    func attachKeystore() {
+        
     }
     
-    
-    
-    func fetchBalance(_ address: String, _ offset:Int) {
-//        print("onFetchgRPCDelegations")
-        DispatchQueue.global().async {
-            let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-            defer { try! group.syncShutdownGracefully() }
-            
-            let channel = self.getConnection(group)
-            defer { try! channel.close().wait() }
-            
-            let req = Cosmos_Bank_V1beta1_QueryAllBalancesRequest.with {
-                $0.address = address
-            }
-            do {
-                let response = try Cosmos_Bank_V1beta1_QueryClient(channel: channel).allBalances(req, callOptions: self.getCallOptions()).response.wait()
-//                print("onFetchgRPCBalance: \(response.balances)")
-                response.balances.forEach { balance in
-                    print(balance)
-                }
-                
-            } catch {
-                print("onFetchgRPCBalance failed: \(error)")
-            }
-        }
-    }
-    
-    func fetchCosmosCoin() {
-//        print("onFetchgRPCDelegations")
+    func refreshBalance() {
         DispatchQueue.global().async {
             let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
             defer { try! group.syncShutdownGracefully() }
@@ -95,20 +67,28 @@ class Cosmos {
                 $0.denom = "uatom"
             }
             do {
-                let response = try Cosmos_Bank_V1beta1_QueryClient(channel: channel).balance(req).response.wait()
+                let response = try Cosmos_Bank_V1beta1_QueryClient(channel: channel).balance(req, callOptions: self.getCallOptions()).response.wait()
 //                print("onFetchgRPCBalance: \(response.balances)")
                 print(response)
                 let balance = response.balance
-                self.subject.send(balance)
+                self.balanceSubject.send(balance)
             } catch {
                 print("onFetchgRPCBalance failed: \(error)")
             }
         }
     }
     
-    func getCallOptions() -> CallOptions {
+    func transfer() {
+        
+    }
+    
+    private func getCallOptions() -> CallOptions {
         var callOptions = CallOptions()
         callOptions.timeLimit = TimeLimit.timeout(TimeAmount.milliseconds(8000))
         return callOptions
+    }
+    
+    private func getConnection(_ group: MultiThreadedEventLoopGroup) -> ClientConnection {
+        return ClientConnection.insecure(group: group).connect(host: provider.host, port: provider.port)
     }
 }
